da0e07ff9735cea48e9e19fbfa7d18a2ef31e9eb
Alice Molin <74822475+Aliceevelina@users.noreply.github.com>
2021-11-12 11:01:37 +0100

    Delete lab1/build directory

--- lab1/build/CMakeFiles/3.19.3/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab1/build/CMakeFiles/3.19.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- lab1/build/TND004-Lab-1.build/Debug/Lab1.build/Objects-normal/x86_64/lab1.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab1/code/lab1.cpp
