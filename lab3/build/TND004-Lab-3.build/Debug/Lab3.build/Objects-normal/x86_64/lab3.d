f9a950077194ecd8d82b81814781587e21cb7007
Alice Molin <74822475+Aliceevelina@users.noreply.github.com>
2021-11-12 11:02:22 +0100

    Delete lab3/build directory

--- lab3/build/TND004-Lab-3.build/Debug/Lab3.build/Objects-normal/x86_64/lab3.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/lab3.cpp
--- lab3/build/TND004-Lab-3.build/Debug/Lab3.build/Objects-normal/x86_64/test1.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/test1.cpp \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/BinarySearchTree.h \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/dsexceptions.h \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/node.h \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab3/code/iterator.h
