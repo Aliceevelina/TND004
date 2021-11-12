2af5c9b7a495274d45ad70408a13c143ed8157df
Alice Molin <74822475+Aliceevelina@users.noreply.github.com>
2021-11-12 11:02:10 +0100

    Delete lab2/build directory

--- lab2/build/TND004-Lab-2.build/Debug/Lab2.build/Objects-normal/x86_64/lab2.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab2/code/lab2.cpp \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab2/code/set.h
--- lab2/build/TND004-Lab-2.build/Debug/Lab2.build/Objects-normal/x86_64/set.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab2/code/set.cpp \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab2/code/set.h \
-  /Users/Alice/Desktop/SKOLA/TND004\ -\ Datastrukturer/lab2/code/node.h
